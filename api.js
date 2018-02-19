f3aefe0f176204dfc68e2426c76fa05e51d08ee7
Kayla Kantola <krkantola@gmail.com>
2018-02-18 21:41:42 -0500

    added POST function for paintings

--- api.js
+++ api.js
@@ -1,15 +1,21 @@
 //REQUIREMENTS
-require(‘dotenv’).config() //connects api.js to your .env file
-const express = require(‘express’) //brings in express…
+require('dotenv').config() //connects api.js to your .env file
+const express = require('express') //brings in express…
 const app = express() //… and runs it.
-const HTTPError = require(‘node-http-error’) // hooray, a way to handle errors!
-const bodyParser = require(‘body-parser’) //brings in body-parser…
+const HTTPError = require('node-http-error') // hooray, a way to handle errors!
+const bodyParser = require('body-parser') //brings in body-parser…
 app.use(bodyParser.json()) //… and runs it.
-const {} = require(‘ramda’)
-const {} = require(‘./dal’)
-const port = propOr(9999, ‘PORT’, process.env) //cool, now you have a port!
+const { propOr, not, isEmpty } = require('ramda')
+const { createPainting } = require('./dal')
+const port = propOr(9999, 'PORT', process.env) //cool, now you have a port!
 const reqFieldChecker = require('./lib/check-req-fields.js')
-const postReqFields = []
+const postReqFields = reqFieldChecker([
+  'name',
+  'movement',
+  'artist',
+  'yearCreated',
+  'museum'
+])
 
 //HOME
 app.get('/', function(req, res, next) {
@@ -17,6 +23,22 @@ app.get('/', function(req, res, next) {
 })
 
 //POST a painting (Crudls)
+app.post('/', function(req, res, next) {
+  const missingFields = postReqFields(req.body)
+
+  if (not(isEmpty(missingFields))) {
+    new HTTPError(400, `Missing Fields: ${join(' ', postReqFields(req.body))}`)
+  }
+
+  createPainting(req.body, function(err, createdPainting) {
+    if (err) {
+      next(err.status, err.message, err)
+      return
+    }
+    res.status(201).send(createdPainting)
+    return
+  })
+})
 
 //GET a painting (cRudls)
 
@@ -28,7 +50,7 @@ app.get('/', function(req, res, next) {
 
 //ERROR HANDLER
 app.use(function(err, req, res, next) {
-  console.log('ERROR!!!', err)
+  console.log('ERROR!', err)
   res.status(err.status || 500)
   res.send(err.message)
 })
--- dal.js
+++ dal.js
@@ -4,11 +4,15 @@ const PouchDB = require('pouchdb-core')
 PouchDB.plugin(require('pouchdb-adapter-http'))
 PouchDB.plugin(require('pouchdb-find'))
 const HTTPError = require('node-http-error') //error handler, yay!
-const slugify = require('slugify') //sweet, a slugify function!
 const db = new PouchDB(process.env.COUCHDB_URL) // most important, this is how you talk to your database!
+const articleKiller = require('./lib/name-adjust.js')
 
 //POST a painting (Crudls)
-
+const createPainting = function(painting, cb) {
+  const name = articleKiller(painting.name)
+  painting._id = `painting_${name}`
+  db.put(painting, cb)
+}
 //GET a painting (cRudls)
 
 //PUT to update a painting (crUdls)
@@ -16,3 +20,7 @@ const db = new PouchDB(process.env.COUCHDB_URL) // most important, this is how y
 //DELETE to delete a painting (cruDls)
 
 //GET to list and search paintings (crudLS)
+
+const dal = { createPainting }
+
+module.exports = dal
