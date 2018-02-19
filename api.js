1fab7f71659d47b81a18e1d9c70a283c60122a17
Kayla Kantola <krkantola@HAL-9000.local>
2018-02-18 20:53:38 -0500

    added dependencies into api and dal

---
+++ api.js
@@ -0,0 +1,36 @@
+//REQUIREMENTS
+require(‘dotenv’).config() //connects api.js to your .env file
+const express = require(‘express’) //brings in express…
+const app = express() //… and runs it.
+const HTTPError = require(‘node-http-error’) // hooray, a way to handle errors!
+const bodyParser = require(‘body-parser’) //brings in body-parser…
+app.use(bodyParser.json()) //… and runs it.
+const {} = require(‘ramda’)
+const {} = require(‘./dal’)
+const port = propOr(9999, ‘PORT’, process.env) //cool, now you have a port!
+
+
+//HOME
+app.get('/', function(req, res, next) {
+  res.send('Welcome to the fanciest museum ever.')
+})
+
+//POST a painting (Crudls)
+
+//GET a painting (cRudls)
+
+//PUT to update a painting (crUdls)
+
+//DELETE to delete a painting (cruDls)
+
+//GET to list and search paintings (crudLS)
+
+//ERROR HANDLER
+app.use(function(err, req, res, next) {
+  console.log('ERROR!!!', err)
+  res.status(err.status || 500)
+  res.send(err.message)
+})
+
+//PORT LISTENER
+app.listen(port, () => console.log('The museum is open at port', port))
--- dal.js
+++ dal.js
@@ -1,1 +1,18 @@
-const PouchDB = require('pouchdb')
+//REQUIREMENTS
+require('dotenv').config() //connects to .env
+const PouchDB = require('pouchdb-core')
+PouchDB.plugin(require('pouchdb-adapter-http'))
+PouchDB.plugin(require('pouchdb-find'))
+const HTTPError = require('node-http-error') //error handler, yay!
+const slugify = require('slugify') //sweet, a slugify function!
+const db = new PouchDB(process.env.COUCHDB_URL) // most important, this is how you talk to your database!
+
+//POST a painting (Crudls)
+
+//GET a painting (cRudls)
+
+//PUT to update a painting (crUdls)
+
+//DELETE to delete a painting (cruDls)
+
+//GET to list and search paintings (crudLS)
